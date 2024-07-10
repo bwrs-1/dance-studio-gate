29fcdbd0a597fb0b78b8efe655d43eb0c8a37824
bwrs-1 <73294528+bwrs-1@users.noreply.github.com>
2024-07-10 05:01:12 +0000

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    { src: '@/plugins/vue-cursor-fx', mode: 'client' },
+    '~/plugins/vue-cursor-fx'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
